eda6cc077deb2daccb7653aa3060a2d6b00be674
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-06-03 22:54:01 +0300

    m

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate an API call
-    setTimeout(() => {
-      resolve("API response");
-    }, 2000);
-  });
+  return new Promise(() => { });
 }
