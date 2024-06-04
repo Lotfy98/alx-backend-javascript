4494cbdd3c228301f10836d14f721f589e7835f7
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-06-05 02:49:51 +0300

    m

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,13 +1,7 @@
 export const weakMap = new WeakMap();
 
-
 const MAX_ENDPOINT_CALLS = 5;
-/**
- * Increments the call count for the given endpoint in the weakMap and throws an error if the call count exceeds the MAX_ENDPOINT_CALLS limit.
- *
- * @param {string} endpoint - The endpoint for which to increment the call count.
- * @throws {Error} If the call count exceeds the MAX_ENDPOINT_CALLS limit.
- */
+
 export function queryAPI(endpoint) {
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 0);
