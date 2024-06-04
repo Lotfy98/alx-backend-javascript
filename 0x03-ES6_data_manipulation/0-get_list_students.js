bbfdea6fa447270a4fa18d8beeb7546425128e31
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-06-05 01:48:07 +0300

    m

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,7 @@
-/**
- * Retrieves a list of students.
- *
- * @return {Array<{id: number, firstName: string, location: string}>} An array of objects representing students,
- * each with properties id (number), firstName (string), and location (string).
- */
 export default function getListStudents() {
-	return [
-		{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-		{ id: 2, firstName: 'James', location: 'Columbia' },
-		{ id: 5, firstName: 'Serena', location: 'San Francisco' },
-	];
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
 }
